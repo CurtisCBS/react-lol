4c1556c708c2af3773fa0f70e91145d6f54a6c91
Curtis <477623879@qq.com>
2017-03-14 09:25:03 +0800

    ui update; rebuild

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,50 +1,74 @@
 import React, { Component } from 'react';
-import MapBackground from './MapBackground';
+// import MapBackground from './MapBackground';
 import List from "./SummonerList";
 class Page extends Component {
   constructor(props) {
     super(props);
     this.state = {
       showList:false,
-      list:null
+      list:null,
+      inputValue:""
     };
   }
 
   render(){
     let boxStyle = {
       width:"100%",
+      backgroundImage:"url("+ require('../images/bg.jpg')+")",
+      height:window.screen.height,
       overflow:"hidden"
     },
+    listBoxStyle = {
+      width:"100%",
+      height:window.screen.height,
+      overflow:"hidden"
+    },
+    inputBoxStyle = {
+      width:"100%",
+      position:"relative",
+      marginTop:"200px"
+    },
+    wrapStyle = {
+      padding:"0 20px"
+    },
     inputStyle = {
-      fontSize:"22px",
+      fontSize:"18px",
       lineHeight:"normal",
       color:"inherit",
       width:"80%",
+      margin:"0 auto",
       display:"block",
-      position:"fixed",
-      left:"10%",
-      top:"30%",
       padding:"10px",
-      marginLeft:"-10px",
       outline:"none",
-      border:"none"
+      border:"none",
+      float:"left"
+    },
+    buttonStyle = {
+      float:"left",
+      width:"20%",
+      marginLeft:"-20px",
+      backgroundColor:"#1c2229",
+      height:"42px",
+      lineHeight:"42px",
+      textAlign:"center"
     },content;
     if(this.state.showList){
-      content = <div>
+      content = <div style={listBoxStyle}>
           <List data={this.state.list}></List>
         </div>
     }
     else{
-      content = <div>
-        <input onKeyUp={this.handlerKeyUp.bind(this)} style={inputStyle} type="text" placeholder="请输入游戏角色昵称"/>
-        <MapBackground></MapBackground>
-      </div>
-    }
-    return (
-        <div style={boxStyle}>
-          {content}
+      content = <div style={boxStyle} className="bg-container">
+        <div style={inputBoxStyle}>
+          <div style={wrapStyle}>
+            <input value={this.state.inputValue} onChange={this.handleChange.bind(this)}  onKeyUp={this.handlerKeyUp.bind(this)} style={inputStyle} type="text" placeholder="请输入游戏角色昵称"/>
+            <div style={buttonStyle} onClick={this.handlerClick.bind(this)} >搜索</div>
+          </div>
         </div>
-    );
+      </div>
+
+    }
+    return content;
   }
 
   handlerKeyUp(event){
@@ -53,6 +77,14 @@ class Page extends Component {
           if(!value) return false;
           this.postRequest(value);
       }
+  }
+
+  handlerClick(event){
+    this.postRequest(this.state.inputValue);
+  }
+
+  handleChange(event){
+    this.setState({inputValue: event.target.value});
   }
 
   postRequest(name){
