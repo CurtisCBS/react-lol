dfb63ac20c27957c4c3e962e04bb72c45300c398
Curtis <477623879@qq.com>
2017-03-14 14:14:47 +0800

    add time format

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 class Battle extends Component {
   constructor(props) {
     super(props);
+    this.addDateFormat();
   }
 
   render(){
@@ -39,6 +40,7 @@ class Battle extends Component {
         if(data.flag_mvp_carry){
           mvp = <span style={mvpStyle}>MVP</span>
         }
+        data.created = this.formatTimeStr(data.created);
     return (
       <div style={boxStyle} className="clearfix">
         <div style={heroStyle}>
@@ -56,6 +58,31 @@ class Battle extends Component {
         </div>
       </div>
     )
+  }
+
+  formatTimeStr(str){
+      return (new Date(str)).Format("yyyy-MM-dd hh:mm:ss");
+  }
+
+  addDateFormat(){
+    Date.prototype.Format = function(fmt)
+    { //author: meizz
+      var o = {
+        "M+" : this.getMonth()+1,                 //月份
+        "d+" : this.getDate(),                    //日
+        "h+" : this.getHours(),                   //小时
+        "m+" : this.getMinutes(),                 //分
+        "s+" : this.getSeconds(),                 //秒
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度
+        "S"  : this.getMilliseconds()             //毫秒
+      };
+      if(/(y+)/.test(fmt))
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+      for(var k in o)
+        if(new RegExp("("+ k +")").test(fmt))
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+      return fmt;
+    }
   }
 }
 
